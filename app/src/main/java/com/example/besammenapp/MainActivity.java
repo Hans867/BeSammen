74e375d3c8636fd97d54bceb8f250001f556f86a
Hans867 <hans867m@stud.kea.dk>
2023-05-17 20:15:38 +0200

    small changes to frontpage testing workflow

--- app/src/main/java/com/example/besammenapp/MainActivity.java
+++ app/src/main/java/com/example/besammenapp/MainActivity.java
@@ -4,22 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import kotlinx.coroutines.Delay;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void beSammen (View view) {
+
+    new Handler().postDelayed(new Runnable() {
+
+        public void run() {
+            Intent intentToHomePageActivity = new Intent(MainActivity.this, HomePageActivity.class);
+            startActivity(intentToHomePageActivity);
+            finish();
+        }
+    }, SPLASH_DELAY);
+
+    /* public void beSammen (View view) {
 
         Intent intentToHomePageActivity = new Intent(this, HomePageActivity.class);
 
         startActivity(intentToHomePageActivity);
     }
 
+     */
 
+    }
 }
